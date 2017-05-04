3156086e76faf20832701fdf29c5ee096e347af3
delvinlow <delvinlowzy@gmail.com>
2017-05-05 00:24:31 +0800

    Add Card and CardSection to display albums

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+// Define as functional component since is just presenting data
+const AlbumDetail = ({ album }) => {
+	const { title, artist, thumbnail_image, image } = album; // destructure
+
 	return (
-		<View>
-			<Text>{props.album.title}</Text>
-		</View>
+		<Card>
+			<CardSection>
+				<View style={styles.thumbnailContainerStyle}>
+					<Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+				</View>
+
+				<View style={styles.headerContentStyle}>
+					<Text style={styles.headerTextStyle}>{title}</Text>
+					<Text>{artist}</Text>
+				</View>
+			</CardSection>
+
+			<CardSection>
+				<Image style={styles.imageStyle} source={{ uri: image }} />
+			</CardSection>
+		</Card>
 	);
+};
+
+// Define styles here because is reusable component. Make them float left
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	headerTextStyle: {
+		fontSize: 18,
+		fontWeight: 'bold'
+	},
+	thumbnailStyle: {
+		height: 50,
+		width: 50
+	}, 
+	thumbnailContainerStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10
+	},
+	imageStyle: {
+		height: 300,
+		flex: 1,
+		width: null
+	}
 };
 
 export default AlbumDetail;

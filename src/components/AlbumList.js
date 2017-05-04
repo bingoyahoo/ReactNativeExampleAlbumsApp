618fb6c6ce564ed81bb4d139d925abd714b29d3a
delvinlow <delvinlowzy@gmail.com>
2017-05-04 19:27:03 +0800

    Add Axios library to issue HTTP calls to retrieve albums data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+
+	state = { albums: [] }; //Just a plain JS object of internal state
+
+	componentWillMount() {
+		// Retrieve data and will call setState() and render() again once done
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+		.then(response => this.setState({ albums: response.data })); 
+	}
+
+	// Helper method to generate Albums.
+	// map() applies fat arrow function to each item in array
+	renderAlbumns() {
+		return this.state.albums.map(album => <Text key={album.title}>{album.title} </Text>);
+	}
+	
+	// since referring JS function
 	render() {
 		return (
 			<View>
-				<Text>Hello</Text>
+				{this.renderAlbumns()} 
 			</View>
 		);
 	}

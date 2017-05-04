12fe7a9c28ca08d7205429a6bf10ddbe3384ac34
delvinlow <delvinlowzy@gmail.com>
2017-05-04 18:02:40 +0800

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
+
+const App = () => (
+  <Text>Some Text </Text>
+);
+
+AppRegistry.registerComponent('albumns', ()=> App);
